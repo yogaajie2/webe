d381a6bf81539e7c3fa28531d2f222286e2200bf
Yoga Ajie <yogaajie2@gmail.com>
2018-05-14 15:15:37 +0700

    added contact

---
+++ kontak.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Font Awesome CSS -->
+    <link href="fontawesome/css/fontawesome-all.css" rel="stylesheet">
+    <!-- Main CSS -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>Webe Piles | Kontak</title>
+  </head>
+  <body>
+    <div class="container">
+
+        <?php include ('navbar.html') ?>
+        
+        <h1 style="text-transform: uppercase;">Kontak</h1>
+        <div class="row">
+            <div class="col-6">
+                <p>Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <form>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Nama">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Nama Perusahaan">
+                    </div>
+                    <div class="form-group">
+                        <input type="tel" class="form-control" placeholder="No. Telepon">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Subjek">
+                    </div>
+                    <div class="form-group">
+                        <textarea class="form-control" rows="5">
+                        </textarea>
+                    </div>
+                </form>
+            </div>
+            <div class="col-6">
+                <div class="row">
+                    <div class="col-6">
+                        <address>
+                            <strong>PT. Webe Piles</strong><br>
+                            Jl. Ki Hajar Dewantara Ruko Golden 8 Blok I No.8 <br>Summarecon Gading Serpong, Pakulonan Bar., Klp. Dua <br> Tangerang, Banten 15810
+                        </address>
+                    </div>
+                    <div class="col-6">
+                        <p>
+                            Telepon: (021) 29238895<br>
+                            Email: webe_piles@yahoo.com
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <img class="img-fluid" src="img/contents/map.png">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
